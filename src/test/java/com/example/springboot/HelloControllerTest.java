298bb6e77052342fd1e80fb8ceeec1e4b44d22d3
Suman Chakraborty <101338574+chakrabortsu@users.noreply.github.com>
2023-04-06 18:01:12 +0530

    Update HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -26,6 +26,6 @@ class HelloControllerTest {
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Welcome to VMware Explore, India !!!"));
+            .andExpect(content().string(""));
     }
 }
